503e8c1644869314918a890057c4d9a31907e95f
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-01-24 16:45:03 +0100

    Exo 2

--- TPs/TP1/tp1_ex2.py
+++ TPs/TP1/tp1_ex2.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
-#
-# A REMPLIR
-#
-# Expression conditionnelle
-# retourne True si x > 0, False si x <= 0 ou si x vaut None
+"""
+ A REMPLIR
+
+ 2. True if Z is None else False
+
+ 3. "sans valeur" if Z is None else "chaine vide" if Z == " " else "autre"
+
+ 4. True if x > 0 else False
+ 	3 -> True
+	-5 -> False
+	None -> Erreur
+	non-défini -> Erreur
+ 
+	None if x is None else True if x > 0 else False
+	3 -> True
+	-5 -> False
+	None -> None
+	non-défini -> Erreur
+
+Expression conditionnelle
+retourne True si x > 0, False si x <= 0 ou si x vaut None
+"""
+
+
 def expression_5(x):
     return x is not None and x > 0
 
